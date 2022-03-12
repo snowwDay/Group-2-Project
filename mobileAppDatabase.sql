518ea9d936dc76fd9babee9beeba63c5c69af650
Janae <45527013+snowwDay@users.noreply.github.com>
2022-03-11 20:36:24 -0600

    Update mobileAppDatabase.sql

--- mobileAppDatabase.sql
+++ mobileAppDatabase.sql
@@ -11,7 +11,7 @@ lastName varchar(200) NOT NULL,
 major varchar(100) NOT NULL, #have to make a list of majors
 gender varchar(50),
 profilePic varchar(200),
-PRIMARY KEY (uID),
+PRIMARY KEY (userID),
 UNIQUE KEY email (email)
 );
 
