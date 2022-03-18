0f012dd91670647a81759bf544990cb4b97356dd
Janae <45527013+snowwDay@users.noreply.github.com>
2022-03-17 21:04:44 -0500

    Update TimeClockManager.sql

--- TimeClockManager.sql
+++ TimeClockManager.sql
@@ -3,14 +3,14 @@ DROP DATABASE IF EXISTS timeClockManager;
 CREATE DATABASE timeClockManager;
 
 CREATE TABLE staff(
-sID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-sName varchar(100) NOT NULL,
-sUserName varchar(100) NOT NULL,
-sPassword varchar(100) NOT NULL
+sID INT PRIMARY KEY AUTO_INCREMENT,
+sName varchar(100),
+sUserName varchar(100),
+sPassword varchar(100)
 );
 
 CREATE TABLE clock(
-id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
 sID INT,
 clockIn DATETIME,
 clockOut DATETIME,
