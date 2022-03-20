741bc4b7ab5356ff6dced438eac33ec7d1b44ebb
DanteOtter <54197896+DanteOtter@users.noreply.github.com>
2022-03-20 22:22:57 +0000

    Fix for inconsistant line spacing

--- API/Main.py
+++ API/Main.py
@@ -32,18 +32,18 @@ def not_found(error):
 @app.route('/update-user', methods=['POST'])
 @auth.login_required
 def update_user():
-	cursor.execute('select count(1) from Users where Username=%s and Password=%s;', (username, password))
+    cursor.execute('select count(1) from Users where Username=%s and Password=%s;', (username, password))
     if cursor.fetchone()[0]:
         info = cursor.fetchone()[0]
-		##Switch to saved sessions, and use sID
+	    ##Switch to saved sessions, and use sID
 	    #cursor.execute('select count(1) from Users where sID=%;', (sID))
 		
 @app.route('/get-user', methods=['GET'])
 @auth.login_required
 def get_user():
-	cursor.execute('select count(1) from Users where Username=%s and Password=%s;', (username, password))
+    cursor.execute('select count(1) from Users where Username=%s and Password=%s;', (username, password))
     if cursor.fetchone()[0]:
-            return make_response(jsonify( { 'Data': str(cursor.fetchone()[0]) } ), 200)
+        return make_response(jsonify( { 'Data': str(cursor.fetchone()[0]) } ), 200)
 
 if __name__ == "__main__":
     import logging
