29e83b87625275673911770aded48745c1ffcdfd
J <45527013+snowwDay@users.noreply.github.com>
2022-03-22 09:10:08 -0500

    Department, ScheduledList, UnscheduledList tables.
    Also updated staff to include department id

--- TimeClockManager.sql
+++ TimeClockManager.sql
@@ -2,11 +2,19 @@ DROP DATABASE IF EXISTS timeClockManager;
 
 CREATE DATABASE timeClockManager;
 
+
+CREATE TABLE department(
+dID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+dName varchar(100)
+);
+
 CREATE TABLE staff(
-sID INT PRIMARY KEY AUTO_INCREMENT,
+sID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+dID INT,
 sName varchar(100) NOT NULL CHECK (sName <> ''),
 sUserName varchar(100) NOT NULL CHECK (sUserName <> ''),
-sPassword varchar(100) NOT NULL CHECK (sPassword <> '')
+sPassword varchar(100) NOT NULL CHECK (sPassword <> ''),
+FOREIGN KEY (dID) REFERENCES department(dID)
 );
 
 CREATE TABLE clock(
@@ -27,6 +35,23 @@ endTime TIME NOT NULL,
 timeOff BOOLEAN, # (on break) 0 for no, 1 for yes
 FOREIGN KEY (sID) REFERENCES staff(sID)
 );
+
+CREATE TABLE scheduleList(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+sID INT,
+dID INT,
+FOREIGN KEY (sID) REFERENCES staff(sID),
+FOREIGN KEY (dID) REFERENCES department(dID)
+);
+
+CREATE TABLE unscheduledList(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+sID INT,
+dID INT,
+FOREIGN KEY (sID) REFERENCES staff(sID),
+FOREIGN KEY (dID) REFERENCES department(dID)
+);
+#might remove dID from the lists, looks redundant.
 
 CREATE TABLE salts(
 id INT PRIMARY KEY AUTO_INCREMENT,
