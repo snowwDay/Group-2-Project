282fd51f7bc44a11906dfd5bf3155449a57ac0c0
DanteOtter <54197896+DanteOtter@users.noreply.github.com>
2022-04-26 02:32:41 +0000

    Update TimeClockManager.sql

--- SQL stuff/TimeClockManager.sql
+++ SQL stuff/TimeClockManager.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS timeClockManager;
 
 CREATE DATABASE timeClockManager;
 
+USE timeClockManager;
 
 CREATE TABLE department(
 dID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -27,8 +28,8 @@ FOREIGN KEY (dID) REFERENCES department(dID)
 CREATE TABLE clock(
 id INT PRIMARY KEY AUTO_INCREMENT,
 sID INT,
-clockIn DATETIME,
-clockOut DATETIME,
+clockIn DATETIME NOT NULL default current_timestamp,
+clockOut DATETIME NOT NULL default current_timestamp,
 status enum('In','Out','Break') NOT NULL,
 FOREIGN KEY (sID) REFERENCES staff(sID)
 );
@@ -43,18 +44,24 @@ timeOff BOOLEAN, # (on break) 0 for no, 1 for yes
 FOREIGN KEY (sID) REFERENCES staff(sID)
 );
 
-CREATE TABLE salts(
+CREATE TABLE staff_salts(
 id INT PRIMARY KEY AUTO_INCREMENT,
 sID INT,
-salt varchar(15) NOT NULL CHECK (salt <> ''),
+salt char(32) NOT NULL CHECK (salt <> ''),
 FOREIGN KEY (sID) REFERENCES staff(sID)
+);
+
+CREATE TABLE admin_salts(
+id INT PRIMARY KEY AUTO_INCREMENT,
+aID INT,
+salt char(32) NOT NULL CHECK (salt <> ''),
+FOREIGN KEY (sID) REFERENCES admin(aID)
 );
 
 CREATE TABLE tokens(
 id INT PRIMARY KEY AUTO_INCREMENT,
 sID INT NOT NULL,
-rank INT NOT NULL,
+ranks INT NOT NULL,
 creationDate DATETIME NOT NULL default current_timestamp,
-token char(32) NOT NULL CHECK (token <> ''),
-FOREIGN KEY (sID) REFERENCES staff(sID)
+token char(32) NOT NULL CHECK (token <> '')
 );
