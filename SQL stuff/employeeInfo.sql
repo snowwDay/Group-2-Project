a3011cec99717b53f84614666e1a1d9ddc802e8a
DanteOtter <54197896+DanteOtter@users.noreply.github.com>
2022-04-26 02:34:43 +0000

    Update employeeInfo.sql

--- SQL stuff/employeeInfo.sql
+++ SQL stuff/employeeInfo.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS employeeInfo;
 
 CREATE DATABASE employeeInfo;
 
+USE employeeInfo;
+
 CREATE TABLE department(
 dID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 dName varchar(100)
@@ -13,7 +15,7 @@ dID INT,
 aEmail varchar(100) NOT NULL,
 aName varchar(100) NOT NULL,
 aUsername varchar(100) NOT NULL,
-aPass varchar(100) NOT NULL,
+aPass varchar(128) NOT NULL,
 FOREIGN KEY (dID) REFERENCES department(dID)
 );
 
@@ -24,8 +26,6 @@ dID INT,
 sEmail varchar(100) NOT NULL,
 sName varchar(100) NOT NULL CHECK (sName <> ''),
 sUserName varchar(100) NOT NULL CHECK (sUserName <> ''),
-sPassword varchar(100) NOT NULL CHECK (sPassword <> ''),
+sPassword varchar(128) NOT NULL CHECK (sPassword <> ''),
 FOREIGN KEY (dID) REFERENCES department(dID)
 );
-
-
