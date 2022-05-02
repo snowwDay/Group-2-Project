13f5e27a4715d83031eddc0442548ff1a7aad843
DanteOtter <54197896+DanteOtter@users.noreply.github.com>
2022-05-02 21:24:23 +0000

    Sync With Server

--- API/Hasher.py
+++ API/Hasher.py
@@ -1,16 +1,14 @@
-class Hasher:
-    def generate_hash(item):
-        salt = generate_salt()
-        salted = salt(item, salt)
-        ##Perform hashing on now salted pass
-    
-    def generate_salt():
-        salt=''
-        ##generate salt for pass
-        return salt
-        
-    def salt(item, salt):
-        ##Salt the password
-        salted=''
-        return salted
+import hashlib
+import os
+import base64
 
+class Hasher:
+    def generate_hash(text, salt):
+        hash = base64.b64encode(hashlib.pbkdf2_hmac(
+            'sha256', # The hash digest algorithm for HMAC
+            text.encode('utf-8'), # Convert the password to bytes
+            salt.encode('utf-8'), # Provide the salt
+            100000, # It is recommended to use at least 100,000 iterations of SHA-256
+            dklen=128 # Get a 128 byte key
+        ))
+        return hash
